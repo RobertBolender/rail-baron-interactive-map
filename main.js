8f844b1cbad5799824ad6c7a8e22c3a9f38b7a9f
Bob Bolender <bobthethree@gmail.com>
2016-11-30 07:34:33 -0600

    Add styles for selected rail line

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 $('document').ready(function(){
 
+  var selectLine = function(){
+    var rr = $('#selector').val();
+    $('.selected').removeClass('selected');
+    $('#'+rr).addClass('selected');
+  };
+
   $('#button').on('click', function(event){
     var rr = $('#selector').val();
     if (!rr){ return; }
     var color = $('#player').val();
     if (color){
-      $('#'+rr).removeClass();
+      $('#'+rr).removeClass('red blue green yellow white black');
       $('#'+rr).addClass(color);
     } else {
-      $('#'+rr).removeClass();
+      $('#'+rr).removeClass('red blue green yellow white black');
     }
+  });
+
+  $('#selector').on('change', function(event){
+    selectLine();
   });
 
   $('path').on('click', function(event){
     $('#selector').val($(this).attr('id'));
+    selectLine();
+  });
+
+  $('#clear').on('click', function(event){
+    $('.selected').removeClass('selected');
   });
 
 });
